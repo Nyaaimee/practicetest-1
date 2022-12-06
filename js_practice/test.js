da2f43041f7aeb209bfdbfcb715fcbde0a594bfa
Nya Nziba Aimee <nyaaimee9@gmail.com>
2022-12-06 03:24:17 -0800

    mastering js

---
+++ js_practice/test.js
@@ -0,0 +1,29 @@
+// Ex1: Write a function expression that sums the elements of an array
+// USING ONLY LOOPING CONSTRUCTS and returns the sum. e.g [2,7,3,6,4] = 22;
+let array = [2,7,3,6,4];
+let total = 0;
+for (let a = 0; a < array.length; a++) {
+   total = total + array[a];   
+}
+console.log('The total is ', total);
+
+
+// Ex2: Using ONLY LOOPS AND STRING-ONLY functions, write a word-converter function
+// that convert words written in snake case to camel case. eg first_name = firstName
+// level_2 = level2.
+let words = '';
+
+
+
+
+// Ex3: Write a function that parses a URL and returns detailed info about the parsed URL.
+// Example; https://googleapis/maps?version=1
+// The above URl can be defined as follows;
+// * protocol = Http
+// * host domain = googleapis
+// * resource param = /maps
+// * query = version=1
+// If the URL is passed to our function, the function should return the above details as
+// KEY-VALUE PAIRS.
+let url = []; 
+
